66f74ef473ff71175bd5e15c774155625f5f1e06
Xinran Che <gracexinran@gmail.com>
2019-05-22 04:13:14 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,8 +1,11 @@
 def reformat_languages(languages)
   # your code here
     hash = {}
+    
     languages.each do |k,v|
+  
       languages[k].each do |language, value|
+        hash[language] = {}
         hash[language][:type] = value[:type]
         hash[language][:style] = k
       end
