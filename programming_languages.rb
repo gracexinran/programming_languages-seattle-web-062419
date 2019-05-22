d3af0ed5136033c6a4b2a30b9336376a2b32057b
Xinran Che <gracexinran@gmail.com>
2019-05-22 04:16:06 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -9,7 +9,7 @@ def reformat_languages(languages)
         hash[language][:type] = value[:type]
         if hash[language][:style]
           hash[language][:style] = []
-        else 
+        else
           hash[language][:style] << k
         end
       end
